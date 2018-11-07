0340127f3ebbc19133e9a60f2a86bf33f9943704
cstornowski <christian.stornowski@diva-e.com>
2018-11-07 15:36:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    rules: {
+        'typescript/adjacent-overload-signatures': 'error',
+        'typescript/class-name-casing': 'error',
+        'typescript/interface-name-prefix': [
+            'error',
+            'always'
+        ],
+        'typescript/no-type-alias': 'error',
+        'typescript/explicit-member-accessibility': 'error',
+        'typescript/member-ordering': 'error',
+        'typescript/no-angle-bracket-type-assertion': 'error',
+        'typescript/no-empty-interface': 'error',
+        'typescript/prefer-namespace-keyword': 'error',
+        'typescript/no-namespace': 'error',
+        'typescript/no-parameter-properties': 'error',
+        'typescript/no-triple-slash-reference': 'error',
+        'typescript/no-var-requires': 'error'
+    }
+};
